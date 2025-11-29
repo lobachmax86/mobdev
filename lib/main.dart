ce6e1567a1a02f9e561a17918cb460bad5bcd288
lobachmax86 <lobachmax86@gmail.com>
2025-11-29 11:43:32 +0300

    feat: add basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Основной каркас экрана
+      body: Column(
+        children: [
+          // Первый контейнер
+          Container(
+            width: double.infinity, // по всей ширине экрана
+            height: 150,
+            color: Colors.blue,
+          ),
+
+          // Ряд с тремя текстами
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // можно поменять на spaceBetween, если нужно
+            children: const [
+              Text('Текст 1'),
+              Text('Текст 2'),
+              Text('Текст 3'),
+            ],
+          ),
+
+          // Второй контейнер
+          Container(
+            width: double.infinity,
+            height: 100,
+            color: Colors.green,
+          ),
+        ],
       ),
     );
   }
